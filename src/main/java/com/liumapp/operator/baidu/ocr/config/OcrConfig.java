2d1a139a7f9fe7b11aebec630f8db090b53fe4ac
niezhiliang <1132263176@qq.com>
2018-12-06 09:08:45 +0800

    ready to test jackson to read yaml file

---
+++ src/main/java/com/liumapp/operator/baidu/ocr/config/OcrConfig.java
@@ -0,0 +1,16 @@
+package com.liumapp.operator.baidu.ocr.config;
+
+/**
+ * file OcrConfig.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/6
+ */
+public class OcrConfig {
+
+
+
+
+}
