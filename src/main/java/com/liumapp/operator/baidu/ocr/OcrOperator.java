d35fdded303b0f5bde147063bcfb0887f24c69de
liumapp <liumapp.com@gmail.com>
2018-12-07 20:38:29 +0800

    ready to add main class

---
+++ src/main/java/com/liumapp/operator/baidu/ocr/OcrOperator.java
@@ -0,0 +1,12 @@
+package com.liumapp.operator.baidu.ocr;
+
+/**
+ * file OcrOperator.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/7
+ */
+public class OcrOperator {
+}
--- src/main/java/com/liumapp/operator/baidu/ocr/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.liumapp.operator.baidu.ocr;
