3dad78a4474f70b67f70ea9339d9caf8fd1080e4
liumapp <liumapp.com@gmail.com>
2018-12-07 20:05:02 +0800

    add test data and ready to test business license

---
+++ src/test/java/com/liumapp/operator/baidu/ocr/BusinessLicenseTest.java
@@ -0,0 +1,22 @@
+package com.liumapp.operator.baidu.ocr;
+
+import org.junit.Test;
+
+/**
+ * file BusinessLicenseTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/7
+ */
+public class BusinessLicenseTest {
+
+    private  String dataPath = "/usr/local/tomcat/project/baidu-ocr-operator/data/";
+
+    @Test
+    public void testBusinessLicense () {
+
+    }
+
+}
