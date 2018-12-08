21b399676544788fbec2fd685667b9774318b37c
liumapp <liumapp.com@gmail.com>
2018-12-08 11:46:23 +0800

    add drive license ocr class

---
+++ src/main/java/com/liumapp/operator/baidu/ocr/dl/DriveLicenseOcr.java
@@ -0,0 +1,12 @@
+package com.liumapp.operator.baidu.ocr.dl;
+
+/**
+ * file DriveLicenseOcr.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/8
+ */
+public class DriveLicenseOcr {
+}
