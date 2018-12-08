11870c8818926ccf3388644e0122a59fda732e68
liumapp <liumapp.com@gmail.com>
2018-12-08 14:43:25 +0800

    add drive license junit test case

---
+++ src/test/java/com/liumapp/operator/baidu/ocr/DriveLicenseTest.java
@@ -0,0 +1,15 @@
+package com.liumapp.operator.baidu.ocr;
+
+/**
+ * file DriveLicenseTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/8
+ */
+public class DriveLicenseTest {
+
+
+
+}
