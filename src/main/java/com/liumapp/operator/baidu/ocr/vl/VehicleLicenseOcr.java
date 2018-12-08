3fc947d7ebf202d492df6e2b2455be1fe2876673
liumapp <liumapp.com@gmail.com>
2018-12-08 14:42:55 +0800

    add vehicle license ocr class

---
+++ src/main/java/com/liumapp/operator/baidu/ocr/vl/VehicleLicenseOcr.java
@@ -0,0 +1,12 @@
+package com.liumapp.operator.baidu.ocr.vl;
+
+/**
+ * file VehicleLicenseOcr.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/8
+ */
+public class VehicleLicenseOcr {
+}
