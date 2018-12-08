3c9f4760609164599680acba209f2b3e27457475
liumapp <liumapp.com@gmail.com>
2018-12-08 14:26:08 +0800

    add dl require

---
+++ src/main/java/com/liumapp/operator/baidu/ocr/dl/require/DriveLicenseOcrRequire.java
@@ -0,0 +1,17 @@
+package com.liumapp.operator.baidu.ocr.dl.require;
+
+import com.liumapp.operator.baidu.ocr.job.JobData;
+
+/**
+ * file DriveLicenseOcrRequire.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/8
+ */
+public class DriveLicenseOcrRequire extends JobData {
+
+
+
+}
