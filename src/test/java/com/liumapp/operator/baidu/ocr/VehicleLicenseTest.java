57c1f93e478e5da8a67057fb75e68f2bb05b510e
liumapp <liumapp.com@gmail.com>
2018-12-08 15:05:15 +0800

    add vehicle license junit test case

---
+++ src/test/java/com/liumapp/operator/baidu/ocr/VehicleLicenseTest.java
@@ -0,0 +1,15 @@
+package com.liumapp.operator.baidu.ocr;
+
+/**
+ * file VehicleLicenseTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/8
+ */
+public class VehicleLicenseTest {
+
+
+
+}
