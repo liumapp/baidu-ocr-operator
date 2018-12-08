edb9a828b7c3ed64b8f38a0f1c03383bb3188bbd
liumapp <liumapp.com@gmail.com>
2018-12-08 15:03:52 +0800

    add vehicle license ocr done

--- src/main/java/com/liumapp/operator/baidu/ocr/vl/VehicleLicenseOcr.java
+++ src/main/java/com/liumapp/operator/baidu/ocr/vl/VehicleLicenseOcr.java
@@ -1,10 +1,13 @@
 package com.liumapp.operator.baidu.ocr.vl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.baidu.aip.ocr.AipOcr;
 import com.liumapp.operator.baidu.ocr.job.JobDetail;
 import com.liumapp.operator.baidu.ocr.vl.require.VehicleLicenseOcrRequire;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * file VehicleLicenseOcr.java
@@ -21,7 +24,17 @@ public class VehicleLicenseOcr extends JobDetail<VehicleLicenseOcrRequire> {
 
     @Override
     public JSONObject handle(VehicleLicenseOcrRequire data) {
+        AipOcr client = new AipOcr(this.ocrConfig.getAppId(),
+                this.ocrConfig.getAppKey(),
+                this.ocrConfig.getAppSecret());
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("detect_direction", "true");
+        options.put("accuracy", "normal");
 
-        return null;
+        // 参数为本地路径
+        org.json.JSONObject res = client.vehicleLicense(data.getLicensePicPath(), options);
+        System.out.println(res.toString(2));
+        return JSON.parseObject(res.toString());
     }
 }
--- src/main/java/com/liumapp/operator/baidu/ocr/vl/require/VehicleLicenseOcrRequire.java
+++ src/main/java/com/liumapp/operator/baidu/ocr/vl/require/VehicleLicenseOcrRequire.java
@@ -11,4 +11,29 @@ import com.liumapp.operator.baidu.ocr.job.JobData;
  * date 2018/12/8
  */
 public class VehicleLicenseOcrRequire extends JobData {
+
+    private String licensePicPath;
+
+    public VehicleLicenseOcrRequire() {
+    }
+
+    public VehicleLicenseOcrRequire(String licensePicPath) {
+        this.licensePicPath = licensePicPath;
+    }
+
+    public String getLicensePicPath() {
+        return licensePicPath;
+    }
+
+    public VehicleLicenseOcrRequire setLicensePicPath(String licensePicPath) {
+        this.licensePicPath = licensePicPath;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleLicenseOcrRequire{" +
+                "licensePicPath='" + licensePicPath + '\'' +
+                '}';
+    }
 }
